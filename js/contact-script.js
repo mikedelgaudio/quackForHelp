d5895a8c4f3aef968252d835043e716468185040
Mike DelGaudio <mikedelgaudio@Mikes-MacBook-Pro.local>
2018-11-29 12:11:51 -0500

    testing some new things

---
+++ js/contact-script.js
@@ -0,0 +1,46 @@
+// Contact Page JS
+//This first function is when you click in the text fields the background changes
+
+function setBackground(e){
+	if (e.type == "focus"){
+		e.target.style.backgroundColor = "#FFE6A2";
+	}else if (e.type == "blur"){
+		e.target.style.backgroundColor = "white";
+	}
+}
+
+window.addEventListener("load", function(){
+	var inputTypeSelectors = "input[type=text], textarea[type=text]";
+	var fields = document.querySelectorAll(inputTypeSelectors);
+	for (i=0; i<fields.length; i++){
+		fields[i].addEventListener("focus", setBackground);
+		fields[i].addEventListener("blur", setBackground);
+	}
+});
+
+
+
+
+var getHerc = document.getElementById("hercFace");
+
+
+getHerc.addEventListener("mouseover", function(){
+	var jokeText = document.createElement("h5");
+	jokeText.setAttribute("class", "quackHeader");
+	jokeText.setAttribute("id", "makeInvis");
+	
+	var makeSpan = document.createElement("span");
+	makeSpan.textContent = "I'm watching you...";
+	jokeText.appendChild(makeSpan);
+	
+	jokeText.style.position = "fixed";
+	jokeText.style.fontSize = "500%";
+	jokeText.style.left = "25%";
+	
+	document.getElementById("test").appendChild(jokeText);
+});
+
+getHerc.addEventListener("mouseleave", function(){
+	test.removeChild(jokeText);
+});
+
