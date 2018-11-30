db54165971fe9334042c663e7a2abdf7f1e1f9d0
Mike DelGaudio <mikedelgaudio@Mikes-MBP.fios-router.home>
2018-11-30 16:49:29 -0500

    working JS for contact form; some CSS changes for contact; quizzes is last thng to do...

--- js/contact-script.js
+++ js/contact-script.js
@@ -35,11 +35,10 @@ getHerc.addEventListener("mouseover", function(){
 	
 	jokeText.style.position = "fixed";
 	jokeText.style.fontSize = "500%";
-	jokeText.style.marginLeft = "25%";
-	
+	jokeText.style.margin = "0% 20% 20% 22%";
 	//jokeText.style.boxShadow = "3px 3px 5px 6px grey";
 	
-	document.getElementById("test").appendChild(jokeText);
+	document.getElementById("mainIdDiv").appendChild(jokeText);
 });
 
 getHerc.addEventListener("mouseleave", function(){
@@ -47,13 +46,59 @@ getHerc.addEventListener("mouseleave", function(){
 	inv.parentNode.removeChild(inv);
 });
 
+
+
+
 function handleFormSubmit(event) {
    // This next line prevents the reload of the form
    event.preventDefault();
    // Get values of inputs
    // Pass values to addNewPost()
-	var username = document.getElementById("name").value;
-	console.log("Name typed: " + username);
+	var nameTyped = document.getElementById("name").value;
+	console.log("Name typed: " + nameTyped);
+	var emailTyped = document.getElementById("email").value;
+	console.log("Email typed: " + emailTyped);
+	
+	addNewPost(nameTyped, emailTyped);
+	
+}
+
+var submittedAlreadyOnce = false;
+
+function addNewPost(nameTyped, emailTyped) {
+	
+	if (submittedAlreadyOnce == false){
+		var parentPostDiv = document.createElement("div");
+		parentPostDiv.setAttribute("id", "post");
+		
+		var namePost = document.createElement("h1");
+		namePost.textContent = "Thank you " + nameTyped + "!";
+		
+		var emailPost = document.createElement("p");
+		emailPost.textContent = "We will contact you at " + emailTyped + " soon!";
+		
+		document.getElementById("contactFormDiv").appendChild(parentPostDiv);
+		
+		submittedAlreadyOnce = true;
+	}else{
+		var inv = document.querySelector('#post');
+		inv.parentNode.removeChild(inv);
+		
+		var parentPostDiv = document.createElement("div");
+		parentPostDiv.setAttribute("id", "post");
+		
+		var namePost = document.createElement("h1");
+		namePost.textContent = "Thank you " + nameTyped + "!";
+		
+		var emailPost = document.createElement("p");
+		emailPost.textContent = "We will contact you at " + emailTyped + " soon!";
+		
+	}
+	
+	parentPostDiv.appendChild(namePost);
+	parentPostDiv.appendChild(emailPost);
+	document.getElementById("contactFormDiv").appendChild(parentPostDiv);
+	
 }
 
 window.onload = () => {
