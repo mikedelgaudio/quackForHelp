9372d124c7712ff1fcfef4bd3c7a821679d0f8b0
Mike DelGaudio <mikedelgaudio@Mikes-MBP.fios-router.home>
2018-11-30 19:34:24 -0500

    removed the inline JS; made the quiz be functional for quiz 1 but trying to find a good way to make all them work

--- js/quizzes-script.js
+++ js/quizzes-script.js
@@ -6,54 +6,63 @@ function handleFormSubmit(event) {
    event.preventDefault();
    // Get values of inputs
    // Pass values to addNewPost()
-	var nameTyped = document.getElementById("name").value;
+	var nameTyped = document.getElementById("quiz1name").value;
 	console.log("Name typed: " + nameTyped);
-	var emailTyped = document.getElementById("email").value;
-	console.log("Email typed: " + emailTyped);
 	
-	addNewPost(nameTyped, emailTyped);
+	addNewPost(nameTyped);
 	
 }
 
 var submittedAlreadyOnce = false;
 
-function addNewPost(nameTyped, emailTyped) {
+function addNewPost(nameTyped) {
 	
-	if (submittedAlreadyOnce == false){
-		var parentPostDiv = document.createElement("div");
-		parentPostDiv.setAttribute("id", "post");
-		
-		var namePost = document.createElement("h1");
-		namePost.textContent = "Thank you " + nameTyped + "!";
-		
-		var emailPost = document.createElement("p");
-		emailPost.textContent = "We will contact you at " + emailTyped + " soon!";
-		
-		document.getElementById("contactFormDiv").appendChild(parentPostDiv);
-		
-		submittedAlreadyOnce = true;
-	}else{
+	if (submittedAlreadyOnce == true){
 		var inv = document.querySelector('#post');
 		inv.parentNode.removeChild(inv);
-		
-		var parentPostDiv = document.createElement("div");
-		parentPostDiv.setAttribute("id", "post");
-		
-		var namePost = document.createElement("h1");
-		namePost.textContent = "Thank you " + nameTyped + "!";
-		
-		var emailPost = document.createElement("p");
-		emailPost.textContent = "We will contact you at " + emailTyped + " soon!";
-		
 	}
 	
+	var parentPostDiv = document.createElement("div");
+		parentPostDiv.setAttribute("id", "post");
+		
+	var namePost = document.createElement("h1");
+		namePost.textContent = "Thank you for taking our quiz " + nameTyped;
+		
+	var responsePost = document.createElement("p");
+		responsePost.setAttribute("class", "createSpaceForText")
+		responsePost.textContent = "After reviewing your submitted answers, we conclude that you should contact us or Herc to get some additional help.";//this could be a var after an if checks the choices...
+	
+	//Add the header thank you and then response to the page...
 	parentPostDiv.appendChild(namePost);
-	parentPostDiv.appendChild(emailPost);
-	document.getElementById("contactFormDiv").appendChild(parentPostDiv);
+	parentPostDiv.appendChild(responsePost);
+	document.querySelector(".Quiz1").appendChild(parentPostDiv);
+	
+	submittedAlreadyOnce = true; //We want to make sure to delete the previous post before making a new one...
+	
+	//Delete the submit button after submission
+	var removeSubmit = document.querySelector("#quiz1Submit");
+		removeSubmit.parentNode.removeChild(removeSubmit);
+	
+	//Create a new button
+	var createReturnHome = document.createElement("button");
+		createReturnHome.setAttribute("class", "takeQuizAgain");
+		createReturnHome.textContent = "Take Another Quiz!";
+	
+	//Add under the text...
+		parentPostDiv.appendChild(createReturnHome);
+		document.querySelector(".Quiz1").appendChild(parentPostDiv);
+	
+	//Make the new button go to the quiz page on click
+	createReturnHome.addEventListener("click", function(){
+		window.location = "quizzes.html";
+	});
 	
 }
 
 window.onload = () => {
    // Once our window is loaded, we add the event listener for our post form
-   contactFormFields.addEventListener('submit', handleFormSubmit);
+    quiz1.addEventListener('submit', handleFormSubmit);
+	quiz2.addEventListener('submit', handleFormSubmit);
+	quiz3.addEventListener('submit', handleFormSubmit);
+	quiz4.addEventListener('submit', handleFormSubmit);
 };
--- js/script_1.js
+++ js/script_1.js
@@ -5,23 +5,30 @@ Brandon Seidman
 Katelyn Chen
 Milad Masoodi
 Mitra Modi*/
-function q1() { 
-	'use strict';
+
+//This is so that when you click on the image it will open the quiz on the page rather than us making 4 pages of different quizzes.
+
+var quiz1Image = document.getElementById("Quiz1");
+quiz1Image.addEventListener("click", function(){
 	document.getElementsByClassName('welcome-page')[0].style.display = 'none';
 	document.getElementsByClassName('Quiz1')[0].style.display = 'flex';
-}
-function q2() { 
-	'use strict';
+});
+
+var quiz2Image = document.getElementById("Quiz2");
+quiz2Image.addEventListener("click", function(){
 	document.getElementsByClassName('welcome-page')[0].style.display = 'none';
 	document.getElementsByClassName('Quiz2')[0].style.display = 'flex';
-}
-function q3() { 
-	'use strict';
+});
+
+var quiz3Image = document.getElementById("Quiz3");
+quiz3Image.addEventListener("click", function(){
 	document.getElementsByClassName('welcome-page')[0].style.display = 'none';
 	document.getElementsByClassName('Quiz3')[0].style.display = 'flex';
-}
-function q4() { 
-	'use strict';
+});
+
+var quiz4Image = document.getElementById("Quiz4");
+quiz4Image.addEventListener("click", function(){
 	document.getElementsByClassName('welcome-page')[0].style.display = 'none';
 	document.getElementsByClassName('Quiz4')[0].style.display = 'flex';
-}
+});
+
