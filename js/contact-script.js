787cfa154c61b3ca4b3365144a78aa4aa22d1220
mmasoodi <miladmasoodi314@gmail.com>
2018-12-01 16:28:32 -0500

    I dont know git so i copied files that i did on another branch, fixed im watching you and did minor work on header and image transforms

--- js/contact-script.js
+++ js/contact-script.js
@@ -1,4 +1,5 @@
 // Contact Page JS
+
 //This first function is when you click in the text fields the background changes
 
 function setBackground(e){
@@ -10,7 +11,7 @@ function setBackground(e){
 }
 
 window.addEventListener("load", function(){
-	var inputTypeSelectors = "input[type=text], textarea[type=text]";
+	var inputTypeSelectors = "input[type=text], textarea[type=text], input[type=email], input[type=tel]";
 	var fields = document.querySelectorAll(inputTypeSelectors);
 	for (i=0; i<fields.length; i++){
 		fields[i].addEventListener("focus", setBackground);
@@ -20,9 +21,9 @@ window.addEventListener("load", function(){
 
 
 
+//The joke when hovering over Herc picture
 
 var getHerc = document.getElementById("hercFace");
-
 
 getHerc.addEventListener("mouseover", function(){
 	var jokeText = document.createElement("h5");
@@ -34,12 +35,12 @@ getHerc.addEventListener("mouseover", function(){
 	jokeText.appendChild(makeSpan);
 	
 	jokeText.style.position = "fixed";
-	jokeText.style.fontSize = "500%";
-
-
-
-	var img = document.getElementById("placer")
-	img.insertBefore(jokeText, img.childNodes[0])
+	// jokeText.style.position = "0,0";
+	// jokeText.style.fontSize = "500%";
+	// jokeText.style.margin = "0% 10% 0% 10%";
+	//jokeText.style.boxShadow = "3px 3px 5px 6px grey";
+	
+	document.getElementById("mainIdDiv").appendChild(jokeText);
 });
 
 getHerc.addEventListener("mouseleave", function(){
@@ -47,3 +48,55 @@ getHerc.addEventListener("mouseleave", function(){
 	inv.parentNode.removeChild(inv);
 });
 
+
+
+//Function that runs on submit will grab the values inputted
+
+function handleFormSubmit(event) {
+   // This next line prevents the reload of the form
+   event.preventDefault();
+   // Get values of inputs
+   // Pass values to addNewPost()
+	var nameTyped = document.getElementById("name").value;
+	//Turn the name enter and make first letter capital
+	nameTyped = nameTyped.charAt(0).toUpperCase() + nameTyped.slice(1);
+	console.log("Name typed: " + nameTyped);
+	var emailTyped = document.getElementById("email").value;
+	console.log("Email typed: " + emailTyped);
+	
+	addNewPost(nameTyped, emailTyped);
+	
+}
+
+//Making the Contact Form Make a New Text Field:
+
+var submittedAlreadyOnce = false;
+
+function addNewPost(nameTyped, emailTyped) {
+	
+	if (submittedAlreadyOnce == true){
+		var inv = document.querySelector('#post');
+		inv.parentNode.removeChild(inv);
+	}
+	
+	var parentPostDiv = document.createElement("div");
+		parentPostDiv.setAttribute("id", "post");
+		
+	var namePost = document.createElement("h1");
+		namePost.textContent = "Thank you " + nameTyped + "!";
+		
+	var emailPost = document.createElement("p");
+		emailPost.textContent = "We will contact you at " + emailTyped + " soon!";
+	
+	parentPostDiv.appendChild(namePost);
+	parentPostDiv.appendChild(emailPost);
+	document.getElementById("contactFormDiv").appendChild(parentPostDiv);
+	submittedAlreadyOnce = true; //We want to make sure to delete the previous post before making a new one...
+	
+}
+
+
+window.onload = () => {
+   // Once our window is loaded, we add the event listener for our post form
+   contactFormFields.addEventListener('submit', handleFormSubmit);
+};
